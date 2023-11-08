a861048fe36f43fc991f9ccfa0cffe930c93173f
Kramer <Mwangala Kramer>
2023-11-08 09:21:35 -0500

    added image

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import evo from "./assets/evo.jpg";
+function Header() {
+  return (
+    <header>
+      <p>Kramer is back</p>
+    </header>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +17,9 @@ function App() {
   return (
     <>
       <div>
+        <Header />
+        <p>Before we start this is our goal</p>
+        <img src={evo} alt="evo" size="300" />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,7 +27,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React=Kramer Job</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
